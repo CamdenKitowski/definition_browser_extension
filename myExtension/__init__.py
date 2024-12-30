4644beac729ae712fc388d1511b44ae5f982fb53
camkit <camdenkitowski@yahoo.com>
2024-12-30 07:37:18 -0800

    able to post

--- myExtension/__init__.py
+++ myExtension/__init__.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask
 from flask_mysqldb import MySQL
 
+from . import definitions
+
 def create_app(test_config=None):
     # flask will look for config files --- this is useful for storing secrets
     app = Flask(__name__, instance_relative_config=True)
@@ -10,12 +12,13 @@ def create_app(test_config=None):
         MYSQL_HOST= 'localhost',
         MYSQL_USER= 'camkit',
         MYSQL_PASSWORD='camkitpass',
-        MYSQL_DB='todo_list_db',
+        MYSQL_DB='definition_db',
     )
 
     # initialize database
     mysql = MySQL(app)
     app.mysql = mysql
+    app.register_blueprint(definitions.bp)
     
     try:
         os.makedirs(app.instance_path)
---
+++ myExtension/definitions.py
@@ -0,0 +1,25 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for, jsonify, current_app
+)
+from werkzeug.exceptions import abort
+from datetime import datetime
+from .db import get_db
+
+bp = Blueprint('definitions', __name__)
+
+@bp.route('/add_definition', methods=['POST'])
+def add_defintion():
+    word = 'other'
+    definition = 'other'
+    try:
+        db = get_db()
+        connection = current_app.mysql.connection
+
+        query = 'INSERT INTO definitions (word, definition) VALUES (%s, %s)'
+        db.execute(query, (word, definition))
+        connection.commit()
+        return jsonify({"message": "Definition added successfully!", "word": word, "definition": definition}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+
