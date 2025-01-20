72b03bc844ce5c08b737be72575a4d9646cb1eed
CamdenKitowski <camdenkitowski@yahoo.com>
2025-01-19 21:28:06 -0800

    more stuff added

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: waitress-serve --port=$PORT flaskBackend:app
+web: gunicorn --workers=4 --bind=0.0.0.0:$PORT flaskBackend:create_app
--- run.py
+++ run.py
@@ -1,9 +1,6 @@
-from waitress import serve
-from flaskBackend import create_app
+# from flaskBackend import create_app
 
-app = create_app()
+# app = create_app()
 
-if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8080)
-
-
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=8080)
