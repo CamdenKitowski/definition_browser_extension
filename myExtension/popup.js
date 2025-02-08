9e652196b9b079596ef22e554167ed5d554baeb3
CamdenKitowski <camdenkitowski@yahoo.com>
2025-02-08 12:37:44 -0800

    update ports

--- myExtension/popup.js
+++ myExtension/popup.js
@@ -1,34 +1,5 @@
 console.log('starting up popup.js');
 
-function uploadtoDatabase(word, definition, pronunciation, pos) {
-    console.log('Button was clicked');
-    fetch('http://127.0.0.1:8080/add_definition', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            word: word,
-            definition: definition,
-            pronunciation: pronunciation,
-            pos: pos
-        })
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        console.log(response, ' the response');
-        return response.json();
-    })
-    .then(data => {
-        console.log('success', data);
-    })
-    .catch(error => {
-        console.error('Error uploading to Flask API:', error);
-    });
-}
-
 document.addEventListener('DOMContentLoaded', function() {
     initializeScrapeButton();
 });
@@ -62,6 +33,36 @@ async function checkDup(word) {
     }
 }
 
+
+function uploadtoDatabase(word, definition, pronunciation, pos) {
+    console.log('Button was clicked');
+    fetch('http://127.0.0.1:8080/add_definition', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            word: word,
+            definition: definition,
+            pronunciation: pronunciation,
+            pos: pos
+        })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        console.log(response, ' the response');
+        return response.json();
+    })
+    .then(data => {
+        console.log('success', data);
+    })
+    .catch(error => {
+        console.error('Error uploading to Flask API:', error);
+    });
+}
+
 async function handleMessageResponse(response) {
     
 
@@ -80,9 +81,6 @@ async function handleMessageResponse(response) {
     const definition = response.definition;
     const pronunciation = response.pronunciation;
     const pos = response.pos;
-
-    
-
 
     const isDup = await checkDup(word);
 
