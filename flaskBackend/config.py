83877809c589420cef40c47e206c46b9ef27f53f
camkit <camdenkitowski@yahoo.com>
2025-04-27 13:41:49 -0700

    update config files + env variables

--- flaskBackend/config.py
+++ flaskBackend/config.py
@@ -1,7 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config:
     # MySQL connection parameters for RDS
-    MYSQL_HOST = '***REDACTED***'
-    MYSQL_USER = '***REDACTED***'
-    MYSQL_PASSWORD = '***REDACTED***'
-    MYSQL_DB = 'definition_db'
+    MYSQL_HOST = os.getenv('MYSQL_HOST')
+    MYSQL_USER = os.getenv('MYSQL_USER')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+    MYSQL_DB = os.getenv('MYSQL_DB')
     MYSQL_CURSORCLASS = 'DictCursor'
